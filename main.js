cb3e858ed18c280514ec65800e5345b4b6c65e97
Samuel Purcell <Samuel.Purcell@tufts.edu>
2013-10-24 23:11:10 -0400

    barebones commit

---
+++ main.js
@@ -0,0 +1,19 @@
+$(function() {
+    /* Data */
+    Todo = Backbone.Model.extend({
+
+    })
+
+    Todos = Backbone.Collection.extend({
+
+    })
+
+    /* Views */
+    TodoList = Backbone.View.extend({
+        
+    })
+
+    TodoListItem = Backbone.View.extend({
+
+    })
+})
